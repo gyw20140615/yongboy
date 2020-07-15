93c465bf1f9f94972190ff8ad6f945aea68bdb9c
gyw20140615 <2327733748@qq.com>
2020-07-15 14:21:42 +0800

    后端代码提交

---
+++ text_project/src/main/java/com/front/text_project/TextProjectApplication.java
@@ -0,0 +1,13 @@
+package com.front.text_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextProjectApplication.class, args);
+    }
+
+}
---
+++ text_project/src/test/java/com/front/text_project/TextProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.front.text_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
